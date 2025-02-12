2621d98d04444f15e9a0577e778307d23ef3c142
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2025-02-12 22:41:06 +1300

    Draw a triangle

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -236,3 +236,152 @@ export function RunDemoAnimation(
         requestAnimationFrame(frame);
     })();
 }
+
+export function RunAnimation(
+    canvas: HTMLCanvasElement,
+    gl: WebGLRenderingContext
+): void {
+    const vertexShader = CreateVertexShader(gl);
+    const fragmentShader = CreateFragmentShader(gl);
+    const program = CreateProgram(gl, vertexShader, fragmentShader);
+    const positionAttributeLocation = gl.getAttribLocation(
+        program,
+        'a_position'
+    );
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // three 2d points
+    const positions = [0, 0, 0, 0.5, 0.7, 0];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    //RENDERING
+
+    const shouldResize = ShouldResizeCanvasToDisplaySize(canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation,
+        size,
+        type,
+        normalize,
+        stride,
+        offset
+    );
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+    gl.drawArrays(primitiveType, offset, count);
+}
+
+function Render(
+    canvas: HTMLCanvasElement,
+    gl: WebGLRenderingContext,
+    program: WebGLProgram
+): void {}
+
+function ShouldResizeCanvasToDisplaySize(canvas: HTMLCanvasElement) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    const needResize =
+        canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+    if (needResize) {
+        // Make the canvas the same size
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+    }
+
+    return needResize;
+}
+
+function CreateProgram(
+    gl: WebGLRenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader
+): WebGLProgram {
+    const program = gl.createProgram()!;
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+    const logInfo = gl.getProgramInfoLog(program);
+    gl.deleteProgram(program);
+
+    throw new Error(logInfo ?? 'Program failed to compile');
+}
+
+function CreateShader(
+    gl: WebGLRenderingContext,
+    type: GLenum,
+    source: string
+): WebGLShader {
+    const shader = gl.createShader(type)!;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    const logInfo = gl.getShaderInfoLog(shader);
+    gl.deleteShader(shader);
+
+    throw new Error(logInfo ?? 'Shader failed to compile');
+}
+
+function CreateVertexShader(gl: WebGLRenderingContext): WebGLShader {
+    return CreateShader(
+        gl,
+        gl.VERTEX_SHADER,
+        `
+// an attribute will receive data from a buffer
+attribute vec4 a_position;
+
+// all shaders have a main function
+void main() {
+
+// gl_Position is a special variable a vertex shader
+// is responsible for setting
+gl_Position = a_position;
+}
+`
+    );
+}
+
+function CreateFragmentShader(gl: WebGLRenderingContext): WebGLShader {
+    return CreateShader(
+        gl,
+        gl.FRAGMENT_SHADER,
+        `
+// fragment shaders don't have a default precision so we need
+// to pick one. mediump is a good default
+precision mediump float;
+
+void main() {
+// gl_FragColor is a special variable a fragment shader
+// is responsible for setting
+gl_FragColor = vec4(1, 0, 0.5, 1); // return reddish-purple
+}
+`
+    );
+}
